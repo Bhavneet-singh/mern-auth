50e05d91892aba61d989a1336e509d0077ad5d0f
Bhavneet Singh <bhavneetsingh9002@gmail.com>
2024-12-28 20:57:14 +0530

    add routes and create pages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Your source files
+    "./public/index.html", // Your HTML file (if applicable)
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
